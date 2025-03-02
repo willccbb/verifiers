44d347076cc77240692a8fe25550d45939dac5ef
jenkins <jenkins@localhost>
2025-03-02 01:26:49 -0500

    peft example

---
+++ verifiers/examples/gsm8k_peft.py
@@ -0,0 +1,48 @@
+import verifiers as vf
+from verifiers.prompts import CODE_PROMPT
+
+model_name = "Qwen/Qwen2.5-7B-Instruct"
+model, tokenizer = vf.get_model_and_tokenizer(model_name)
+
+vf_env = vf.CodeEnv(dataset="gsm8k", few_shot=[], system_prompt=CODE_PROMPT)
+dataset = vf_env.get_dataset()
+eval_dataset = vf_env.get_eval_dataset(n=20)
+rubric = vf_env.get_rubric()
+
+# notable defaults: lr = 1e-6, max_grad_norm = 0.01, constant lr 10 warmup steps, 1024 tokens in+out
+run_name = "gsm8k-code_" + model_name.split("/")[-1].lower()
+training_args = vf.get_default_grpo_config(run_name=run_name, num_gpus=8)
+# rollouts per prompt
+training_args.num_generations = 7
+# minibatch size per GPU ( bs 6 * 7 gpus / 7 rollouts -> 6 prompts per batch)
+training_args.per_device_train_batch_size = 6
+# batches to accumulate (6 prompts * 4 -> 24 prompts per global batch)
+training_args.gradient_accumulation_steps = 4
+# steps per global batch (1 on-policy, 1 off-policy)
+training_args.num_iterations = 2
+# no ref model
+training_args.beta = 0.0
+# evals
+training_args.eval_strategy = "steps"
+training_args.per_device_eval_batch_size = 8
+training_args.eval_steps = 100
+# lora
+from peft import LoraConfig # type: ignore
+peft_config = LoraConfig(
+    r=16,
+    lora_alpha=64,
+    lora_dropout=0.05,
+    target_modules=["q_proj", "k_proj", "v_proj"]
+)
+
+trainer = vf.GRPOEnvTrainer(
+    model=model,
+    processing_class=tokenizer,
+    reward_funcs=rubric, 
+    env=vf_env,
+    args=training_args,
+    train_dataset=dataset,
+    eval_dataset=eval_dataset,
+    peft_config=peft_config
+)
+trainer.train()
