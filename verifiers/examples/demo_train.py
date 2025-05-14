5d9c9ae937333140501d7af28f798283156c537c
William Brown <williambrown97@gmail.com>
2025-05-14 18:08:25 +0000

    dev

--- verifiers/examples/demo_train.py
+++ verifiers/examples/demo_train.py
@@ -1,23 +1,22 @@
-from datasets import concatenate_datasets
+from peft import LoraConfig
 from trl import GRPOConfig
 
 import verifiers as vf
-from verifiers.tools import search, python, ask
+from verifiers.tools import python
 from verifiers.utils import preprocess_dataset
 
+model_name = "Qwen/Qwen3-1.7B"
+
 """
-Multi-GPU training (single node, 4 training + 4 inference)
+2-GPU training (single node, 1 training + 1 inference)
 
-CUDA_VISIBLE_DEVICES=0,1,2,3 python verifiers/inference/vllm_serve.py --model 'Qwen/Qwen2.5-7B-Instruct' \
-    --tensor_parallel_size 4 --max_model_len 8192 --dtype bfloat16 \
-    --gpu_memory_utilization 0.9 --enable_prefix_caching True \
-    --host 0.0.0.0 --port 8000
+CUDA_VISIBLE_DEVICES=0 python verifiers/inference/vllm_serve.py --model 'Qwen/Qwen3-1.7B' --max_model_len 2048 --dtype bfloat16 --gpu_memory_utilization 0.95 --enable_prefix_caching True
 
-CUDA_VISIBLE_DEVICES=4,5,6,7 accelerate launch --config-file configs/zero3.yaml verifiers/examples/math_train.py
+CUDA_VISIBLE_DEVICES=1 accelerate launch --num-processes 1 --config-file configs/zero3.yaml verifiers/examples/demo_train.py
 """
 
 TOOL_PROMPT = """
-Think step-by-step inside <reasoning>...</reasoning> tags, then either call a tool inside <tool>...</tool> tags, or give your final answer inside <answer>...</answer> tags.
+Think step-by-step inside <think>...</think> tags, then either call a tool inside <tool>...</tool> tags, or give your final answer inside <answer>...</answer> tags.
 
 You have access to the following tools to help solve problems:
 
@@ -35,62 +34,42 @@ Example usage:
 You will then see the tool's output inside <result> tags. You may call tools multiple times if needed.
 
 The <answer>...</answer> tags should contain only your final answer.
-
-Example for multiple choice questions:
-<answer>
-A
-</answer>
-
-Example for math problems:
-<answer>
-\\frac{{1}}{{2}}
-</answer>
 """
 
-dataset = preprocess_dataset("math", "train", n=6000)
-
-eval_aime24 = preprocess_dataset("aime2024", n=30)
-eval_aime25 = preprocess_dataset("aime2025", n=30)
-eval_dataset = concatenate_datasets([eval_aime24, eval_aime25]).shuffle(seed=0)
+dataset = preprocess_dataset("math", "train", n=1000)
 
 vf_env = vf.ToolEnv(
     dataset=dataset,
-    eval_dataset=eval_dataset,
     system_prompt=TOOL_PROMPT,
     few_shot=[],
     tools=[python],
-    max_steps=5
+    llm_fields=["think", ("tool", "answer")],
+    env_fields=["result"],
+    max_steps=3
 )
 print(vf_env.system_prompt)
 
-model_name = "Qwen/Qwen2.5-1.5B-Instruct"
 model, tokenizer = vf.get_model_and_tokenizer(model_name)
-run_name = "math-grpo_" + model_name.split("/")[-1].lower()
+run_name = "demo-grpo_" + model_name.split("/")[-1].lower()
 
 training_args=GRPOConfig(
     output_dir=f"outputs/{run_name}",
     run_name=run_name,
     learning_rate=1e-6,
-    lr_scheduler_type="constant_with_warmup",
-    warmup_steps=10,
+    lr_scheduler_type="constant",
     num_train_epochs=1,
     temperature=1.0,
-    max_steps=1000,
+    max_steps=100,
     bf16=True,
     max_grad_norm=0.1,
-    num_iterations=2,
-    beta=0.002,
+    num_iterations=1,
+    beta=0,
     max_prompt_length=1024,
-    max_completion_length=2048,
-    per_device_train_batch_size=12,
-    per_device_eval_batch_size=12,
-    num_generations=6,
+    max_completion_length=1024,
+    per_device_train_batch_size=16,
+    num_generations=4,
     gradient_accumulation_steps=1,
     gradient_checkpointing=True,
-    eval_strategy="steps",
-    eval_steps=100,
-    eval_accumulation_steps=1,
-    eval_on_start=False,
     save_strategy="steps",
     save_steps=100,
     save_only_model=True,
@@ -104,6 +83,14 @@ training_args=GRPOConfig(
     report_to="wandb",
     reward_weights=vf_env.get_reward_weights()
 )
+
+peft_config = LoraConfig(
+    r=16,
+    lora_alpha=64,
+    target_modules=["q_proj", "v_proj", "k_proj", "o_proj", "gate_proj", "down_proj", "up_proj"],
+    task_type="CAUSAL_LM",
+)
+
 trainer = vf.GRPOEnvTrainer(
     model=model,
     processing_class=tokenizer,
@@ -111,6 +98,7 @@ trainer = vf.GRPOEnvTrainer(
     env=vf_env,
     args=training_args,
     train_dataset=vf_env.get_dataset(),
-    eval_dataset=vf_env.get_eval_dataset()
+    eval_dataset=vf_env.get_eval_dataset(),
+    peft_config=peft_config
 )
-trainer.train() 
+trainer.train()
