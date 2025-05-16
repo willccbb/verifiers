c4f0e62bbf86e01ca6d35e766046bd14182236d5
William Brown <williambrown97@gmail.com>
2025-05-16 23:07:05 +0000

    train.py

---
+++ train.py
@@ -0,0 +1,75 @@
+from datasets import load_dataset
+import verifiers as vf
+
+#model = 'Qwen/Qwen2.5-1.5B-Instruct'
+model_name = "willcb/Qwen2.5-1.5B-Reverse-SFT"
+dataset = load_dataset('agentlans/wikipedia-paragraphs', split='train').map(lambda x: {'question': x['text'], 'answer': x['text'][::-1]})
+parser = vf.XMLParser(['think', 'answer'], answer_field='answer')
+system_prompt = f"""Reverse the given text.
+
+Respond in the following format:
+{parser.get_format_str()}"""
+
+def lcs_reward_func(completions, answer, **kwargs) -> list[float]:
+    """
+    LCS ratio of the reversed prompt and the parsed completion.
+    """
+    def lcs_ratio(x: str, y: str) -> float:
+        """
+        Return the longest common subsequence ratio of x and y.
+        """
+        from difflib import SequenceMatcher
+        return SequenceMatcher(None, x, y).ratio()
+    responses = [parser.parse_answer(c) or '' for c in completions]
+    return [lcs_ratio(r, a) for r, a in zip(responses, answer)]
+
+rubric = vf.Rubric(funcs=[
+	lcs_reward_func,
+	parser.get_format_reward_func(),
+], weights=[1.0, 0.2])
+
+vf_env = vf.SingleTurnEnv(
+    dataset=dataset, # type: ignore
+    system_prompt=system_prompt,
+    parser=parser,
+    rubric=rubric
+)
+
+run_name = "rev_warmup_grpo_fft"
+model, tokenizer = vf.get_model_and_tokenizer(model_name)
+
+from trl import GRPOConfig
+args = GRPOConfig(
+    output_dir=f"outputs/{run_name}",
+    run_name=run_name,
+    learning_rate=1e-6,
+    lr_scheduler_type="constant",
+    num_train_epochs=1,
+    max_steps=500,
+    bf16=True,
+    max_grad_norm=0.1,
+    num_iterations=2,
+    max_prompt_length=1024,
+    max_completion_length=2048,
+    per_device_train_batch_size=16,
+    num_generations=8,
+    gradient_accumulation_steps=1,
+    gradient_checkpointing=True,
+    save_strategy="steps",
+    save_steps=500,
+    save_only_model=True,
+    use_vllm=True,
+    logging_steps=1,
+    log_on_each_node=False,
+    log_completions=True,
+    report_to="wandb",
+)
+
+model, tokenizer = vf.get_model_and_tokenizer(model_name)
+trainer = vf.GRPOEnvTrainer(
+    model=model,
+    processing_class=tokenizer,
+    env=vf_env,
+    args=args
+)
+trainer.train()
