3ccf892a012b994da4e9d68d7346c9234f018cd8
William Brown <williambrown97@gmail.com>
2025-05-22 20:24:56 +0000

    reverse

--- verifiers/examples/warmup/data_reverse.py
+++ verifiers/examples/warmup/data_reverse.py
@@ -5,41 +5,42 @@ from datasets import load_dataset
 
 import verifiers as vf
 
+dataset = load_dataset('agentlans/wikipedia-paragraphs', split='train')
+dataset = dataset.map(lambda x: {'question': x['text'], 'answer': x['text'][::-1]})
+
+parser = vf.XMLParser(['think', 'answer'], answer_field="answer")
+system_prompt = f"""Respond in the following format:
+{parser.get_format_str()}
+
+Reverse the given text character-by-character."""
+
+def lcs_reward_func(completion, answer, **kwargs) -> float:
+    """
+    LCS ratio of the reversed prompt and the parsed completion.    
+    """
+    def lcs_ratio(x: str, y: str) -> float:
+        """
+        Return the longest common subsequence ratio of x and y.
+        """
+        from difflib import SequenceMatcher
+        return SequenceMatcher(None, x, y).ratio()
+    response = parser.parse_answer(completion) or ''
+    return lcs_ratio(response, answer)
+
+rubric = vf.Rubric(funcs=[
+    lcs_reward_func,
+    parser.get_format_reward_func(),
+], weights=[1.0, 0.2])
+
+vf_env = vf.SingleTurnEnv(
+    eval_dataset=dataset,
+    system_prompt=system_prompt,
+    parser=parser,
+    rubric=rubric,
+    max_concurrent=10
+)
+
 def main(api: str, num_samples: int, max_tokens: int):
-    dataset = load_dataset('agentlans/wikipedia-paragraphs', split='train')
-    dataset = dataset.map(lambda x: {'question': x['text'], 'answer': x['text'][::-1]})
-
-    parser = vf.XMLParser(['think', 'answer'], answer_field="answer")
-    system_prompt = f"""Respond in the following format:
-    {parser.get_format_str()}
-
-    Reverse the given text character-by-character."""
-
-    def lcs_reward_func(completion, answer, **kwargs) -> float:
-        """
-        LCS ratio of the reversed prompt and the parsed completion.    
-        """
-        def lcs_ratio(x: str, y: str) -> float:
-            """
-            Return the longest common subsequence ratio of x and y.
-            """
-            from difflib import SequenceMatcher
-            return SequenceMatcher(None, x, y).ratio()
-        response = parser.parse_answer(completion) or ''
-        return lcs_ratio(response, answer)
-
-    rubric = vf.Rubric(funcs=[
-        lcs_reward_func,
-        parser.get_format_reward_func(),
-    ], weights=[1.0, 0.2])
-
-    vf_env = vf.SingleTurnEnv(
-        eval_dataset=dataset,
-        system_prompt=system_prompt,
-        parser=parser,
-        rubric=rubric,
-        max_concurrent=10
-    )
     # collect V3/R1 rollouts from API
     if api == "deepseek":
         base_url = "https://api.deepseek.com"
