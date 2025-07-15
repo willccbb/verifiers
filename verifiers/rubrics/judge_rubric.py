fea94fdc73971cfd7615549c60596d53fca2e750
William Brown <williambrown97@gmail.com>
2025-07-15 20:46:24 +0000

    judgerubric refactor

--- verifiers/rubrics/judge_rubric.py
+++ verifiers/rubrics/judge_rubric.py
@@ -28,6 +28,7 @@ Respond either "yes" or "no" only."""
 class JudgeRubric(Rubric):
     def __init__(
         self,
+        parser: Parser = Parser(),
         judge_client: OpenAI | None = None,
         judge_model: str = "gpt-4.1-nano",
         judge_sampling_args: dict = {},
@@ -35,19 +36,18 @@ class JudgeRubric(Rubric):
         judge_score_fn: Callable[[str], float] = lambda x: 1.0
         if "yes" in x.lower()
         else 0.0,
-        parser: Parser = Parser(),
         **kwargs,
     ):
         super().__init__(**kwargs)
+        self.parser = parser
         self.judge_client = judge_client if judge_client is not None else OpenAI()
         self.judge_model = judge_model
         self.judge_prompt = judge_prompt
         self.judge_sampling_args = judge_sampling_args
         self.judge_score_fn = judge_score_fn
-        self.parser = parser
         self.add_reward_func(self.judge_reward_func)
 
-    def judge_reward_func(self, prompt, completion, answer, **kwargs) -> float:
+    def judge(self, prompt, completion, answer, **kwargs) -> str:
         if isinstance(prompt, list):
             question = prompt[-1]["content"]
         else:
@@ -62,4 +62,8 @@ class JudgeRubric(Rubric):
             **self.judge_sampling_args,
         )
         judge_response = str(judge_response.choices[0].message.content)
+        return judge_response
+
+    def judge_reward_func(self, prompt, completion, answer, **kwargs) -> float:
+        judge_response = self.judge(prompt, completion, answer, **kwargs)
         return self.judge_score_fn(judge_response)
