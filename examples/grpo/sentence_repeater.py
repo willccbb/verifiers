02fd6cd99ca1cae1c636a886d8ce15b7d86746e6
William Brown <williambrown97@gmail.com>
2025-07-19 21:54:28 +0000

    sentence repeater grpo

---
+++ examples/grpo/sentence_repeater.py
@@ -0,0 +1,37 @@
+import verifiers as vf
+
+"""
+inference:
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5 vf-vllm --model willcb/Qwen2.5-1.5B --data-parallel-size 6 --enforce-eager
+
+training:
+CUDA_VISIBLE_DEVICES=6,7 accelerate launch --config-file configs/zero3.yaml --num-processes 2 examples/grpo/sentence_repeater.py
+"""
+
+model_name = "willcb/Qwen3-4B-Wordle"
+model, tokenizer = vf.get_model_and_tokenizer(model_name)
+
+vf_env = vf.load_environment(env_id="wordle", use_think=True)
+
+run_name = "wordle-grpo-4b"
+training_args = vf.grpo_defaults(run_name=run_name)
+training_args.per_device_train_batch_size = 8
+training_args.num_generations = 16
+training_args.gradient_accumulation_steps = 8
+training_args.max_tokens = 1024  # per turn
+training_args.max_seq_len = 4096
+training_args.max_steps = 200
+training_args.eval_strategy = "steps"
+training_args.eval_steps = 20
+training_args.mask_env_responses = True
+training_args.max_grad_norm = 0.1
+training_args.beta = 0.0
+training_args.async_generation_timeout = 600
+
+trainer = vf.GRPOTrainer(
+    model=model,
+    processing_class=tokenizer,
+    env=vf_env,
+    args=training_args,
+)
+trainer.train()
