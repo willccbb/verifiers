18feee9952db364964c15286cfa27945ee5ddbd2
William Brown <williambrown97@gmail.com>
2025-07-26 02:44:06 +0000

    math-python defaults

--- examples/grpo/train_math_python.py
+++ examples/grpo/train_math_python.py
@@ -8,7 +8,7 @@ vf-install math-python (-p /path/to/environments)
 vf-eval math-python (-m model_name in endpoints.py)
 
 # inference
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5 vf-vllm --model 'willcb/Qwen3-4B' \
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5 vf-vllm --model 'willcb/Qwen3-1.7B' \
     --data-parallel-size 6 --enforce-eager --disable-log-requests
 
 # training
@@ -23,15 +23,21 @@ model, tokenizer = vf.get_model_and_tokenizer(model_name)
 run_name = "math-python_" + model_name.split("/")[-1].lower()
 
 training_args = vf.grpo_defaults(run_name=run_name)
-training_args.num_iterations = 2
 training_args.per_device_train_batch_size = 8
-training_args.num_generations = 8
-training_args.gradient_accumulation_steps = 2
+training_args.num_generations = 16
+training_args.gradient_accumulation_steps = 8
+training_args.max_tokens = 2048
+training_args.max_seq_len = 4096
+training_args.max_steps = 200
+training_args.mask_env_responses = True
+training_args.max_grad_norm = 0.1
+training_args.beta = 0.1
 
 trainer = vf.GRPOTrainer(
     model=model,
     processing_class=tokenizer,
     env=vf_env,
     args=training_args,
+    # lora_config=vf.lora_defaults()
 )
 trainer.train()
