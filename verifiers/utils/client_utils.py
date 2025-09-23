7a953b41e6499ad13ad2f2100984bafc705f5d36
Stefano Fiorucci <stefanofiorucci@gmail.com>
2025-09-23 02:36:15 +0200

    fix typos (#356)

--- verifiers/utils/client_utils.py
+++ verifiers/utils/client_utils.py
@@ -8,13 +8,13 @@ from openai import AsyncOpenAI
 def setup_client(
     api_base_url: str,
     api_key_var: str,
-    timeout: float = 600.0,  # OAI default, larger value recommened for evals
-    max_connections: int = 1000,  # OAI default, larger value recommened for evals
-    max_keepalive_connections: int = 100,  # OAI default, larger value recommened for evals
-    max_retries: int = 2,  # OAI default, larger value recommened for evals
+    timeout: float = 600.0,  # OAI default, larger value recommended for evals
+    max_connections: int = 1000,  # OAI default, larger value recommended for evals
+    max_keepalive_connections: int = 100,  # OAI default, larger value recommended for evals
+    max_retries: int = 2,  # OAI default, larger value recommended for evals
 ) -> AsyncOpenAI:
     """
-    A helper function to setup an
+    A helper function to setup an AsyncOpenAI client.
     """
     # Setup timeouts and limits
     http_timeout = httpx.Timeout(timeout, connect=5.0)
